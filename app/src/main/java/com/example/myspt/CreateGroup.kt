5c756b4f2fc61e6b1b1b0bcaebe09065487f52a3
Acer <phattaraporn1309@gmail.com>
2026-02-16 18:01:13 +0700

    เพิ่ม craetegroup

--- app/src/main/java/com/example/myspt/CreateGroup.kt
+++ app/src/main/java/com/example/myspt/CreateGroup.kt
@@ -1,37 +1,20 @@
 package com.example.myspt
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class BillSplit : AppCompatActivity() {
-    var btnBack: ImageView? = null
-    var btnSplit: AppCompatButton? = null
+class CreateGroup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bill_split)
+        setContentView(R.layout.activity_create_group)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        init()
-        btnBack!!.setOnClickListener {
-            finish()
-        }
-
-        btnSplit!!.setOnClickListener {
-
-        }
-    }
-    private fun init() {
-        btnBack = findViewById(R.id.btnBackF)
-
-        btnSplit = findViewById(R.id.btnSplit)
     }
 }
