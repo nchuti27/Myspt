c3fbad149cdc7fabeaca0ca5f837ff4a6302f7c6
Acer <phattaraporn1309@gmail.com>
2026-02-16 19:56:27 +0700

    แก้สปริดบิลแล้วจ่ะพี่จ๋า

--- app/src/main/java/com/example/myspt/DebtSummary.kt
+++ app/src/main/java/com/example/myspt/DebtSummary.kt
@@ -1,12 +1,20 @@
 package com.example.myspt
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class DebtSummary : AppCompatActivity() {
+    var btnBack: ImageView? = null
+    var tabFriends: TextView? = null
+    var rvDebts: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,21 @@ class DebtSummary : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        init()
+
+        btnBack?.setOnClickListener {
+            finish()
+        }
+        tabFriends?.setOnClickListener {
+            val intent = Intent(this, FriendOwe::class.java)
+            val options = android.app.ActivityOptions.makeCustomAnimation(this, 0, 0)
+            startActivity(intent, options.toBundle())
+            finish()
+        }
+    }
+    private fun init() {
+        btnBack = findViewById(R.id.imageView)
+        tabFriends = findViewById(R.id.tabFriends)
+        rvDebts = findViewById(R.id.rvDebts)
     }
 }
--- app/src/main/java/com/example/myspt/FriendOwe.kt
+++ app/src/main/java/com/example/myspt/FriendOwe.kt
@@ -1,12 +1,21 @@
 package com.example.myspt
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class FriendOwe : AppCompatActivity() {
+
+    var btnBack: ImageView? = null
+    var tabItems: TextView? = null
+    var rvFriendOwe: RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,22 @@ class FriendOwe : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        init()
+
+        btnBack?.setOnClickListener {
+            finish()
+        }
+        tabItems?.setOnClickListener {
+            val intent = Intent(this, DebtSummary::class.java)
+            val options = android.app.ActivityOptions.makeCustomAnimation(this, 0, 0)
+            startActivity(intent, options.toBundle())
+            finish()
+        }
+    }
+
+    private fun init() {
+        btnBack = findViewById(R.id.btnBack)
+        tabItems = findViewById(R.id.tabItems)
+        rvFriendOwe = findViewById(R.id.rvFriendOwe)
     }
 }
