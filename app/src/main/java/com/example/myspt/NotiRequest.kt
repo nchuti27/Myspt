4962bf8e03dc6d52d5fd63967c1caba90f11f4a3
Acer <phattaraporn1309@gmail.com>
2026-02-23 14:24:09 +0700

    แก้noti 3 tab

---
+++ app/src/main/java/com/example/myspt/NotiRequest.kt
@@ -0,0 +1,20 @@
+package com.example.myspt
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class NotiRequest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_noti_request)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
